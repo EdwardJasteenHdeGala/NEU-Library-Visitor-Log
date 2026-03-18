93c394252eba1ab988000792136db35db8561920
Edward Jasteen H. de Gala <edwardjasteen.degala@neu.edu.ph>
2026-03-18 18:17:44 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,6 @@ export const firebaseConfig = {
   "appId": "1:941329290095:web:ace8a627bd718978d9ce9a",
   "apiKey": "AIzaSyC4Zy3Z6pfokjBP4eD9zzq1JW-f1h1rHLo",
   "authDomain": "studio-3349078912-96ccd.firebaseapp.com",
-  "measurementId": "",
+  "measurementId": "G-61G5T7H93S",
   "messagingSenderId": "941329290095"
 };
