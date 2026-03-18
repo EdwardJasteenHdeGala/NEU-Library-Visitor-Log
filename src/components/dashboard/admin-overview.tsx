2e28b63461ab12dde34a7b9adf5ca03c3b64a988
Edward Jasteen H. de Gala <edwardjasteen.degala@neu.edu.ph>
2026-03-18 17:48:31 +0000

    resolve the error

--- src/components/dashboard/admin-overview.tsx
+++ src/components/dashboard/admin-overview.tsx
@@ -8,7 +8,6 @@ import {
   LayoutDashboard, 
   Activity,
   Clock,
-  XCircle,
   AlertCircle
 } from "lucide-react";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
